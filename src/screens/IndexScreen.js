632e696ea13ed810e1c404817728aee76af92f2a
Ctnn <yigitcetinkaya2002@hotmail.com>
2022-07-21 14:19:20 +0300

    Style Sheets

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,7 @@
 import React,{useContext} from 'react';
 import {View,Text,StyleSheet,FlatList,Button } from 'react-native';
 import {Context} from './context/BlogContext.js';
+import {Feather} from '@expo/vector-icons';
 
 
 const IndexScreen = () => {
@@ -8,20 +9,37 @@ const IndexScreen = () => {
   
     return (
       <View>
-        <Text>Index Screen</Text>
+       
         <Button title="Add Post" onPress={addBlogPost} />
         <FlatList
           data={state}
           keyExtractor={blogPost => blogPost.title}
           renderItem={({ item }) => {
-            return <Text>{item.title}</Text>;
+            return (<View style={styles.row}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Feather  style={styles.icon} name="trash"/>
+                </View>);
           }}
         />
       </View>
     );
   };
   
-  const styles = StyleSheet.create({});
+  const styles = StyleSheet.create({
+    row:{
+            flexDirection:'row', //Same Line Settings
+            justifyContent:'space-between',
+            paddingVertical:20,
+            borderTopWidth:1,
+            borderColor:'gray',
+    },
+    title:{
+        fontSize:18,
+    },
+    icon:{
+        fontSize:24,
+    },
+  });
   
   export default IndexScreen;
 
--- src/screens/context/createDataContext.js
+++ src/screens/context/createDataContext.js
@@ -1,21 +1,25 @@
-import React,{useReducer, userReducer} from 'react';
+import React, { useReducer } from 'react';
 
-export default(reducer,actions,initialState)=>{
+export default (reducer, actions, initialState) => {
+  const Context = React.createContext();
 
-const Context=React.createContext();
-
-const Provider = ({children})=> {
-
-    const [state,dispatch]=useReducer(reducer,initialState);
+  const Provider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
 /*
     const editBlogPost =()=>{ }
     const deleteBlogPost=()=>{}  Way to use easier and better then this  'useReducer'*/
+    // actions === { addBlogPost: (dispatch) => { return () => {} } }
+    const boundActions = {};
+    for (let key in actions) {
+      boundActions[key] = actions[key](dispatch);
+    }
 
-    return <Context.Provider value={{state: state}}>
+    return (
+      <Context.Provider value={{ state, ...boundActions }}>
         {children}
-    </Context.Provider>
-};
+      </Context.Provider>
+    );
+  };
 
-return {Context,Provider};
-
+  return { Context, Provider };
 };
